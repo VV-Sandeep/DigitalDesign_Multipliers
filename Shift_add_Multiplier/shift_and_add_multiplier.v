d050bc723d99b6c24c9c09a9c5508925ccf0a0d1
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-09-08 23:47:41 +0530

    Update shift_and_add_multiplier.v

--- Shift_add_Multiplier/shift_and_add_multiplier.v
+++ Shift_add_Multiplier/shift_and_add_multiplier.v
@@ -1,1 +1,32 @@
-
+module shift_and_add_multiplier_nbit(a,b,clk,rst,out);
+parameter n = 8;
+input [n-1:0]a,b;
+input clk,rst;
+output reg [2*n:0]out;
+reg c;
+reg [n-1:0]acc;
+reg [3:0]N;
+reg [n-1:0]m,q;
+always @(posedge clk)
+begin
+    if (rst)
+    begin
+    m = a;
+    q = b;
+    c = 0;
+    acc = 0;
+    N = n;
+    end
+    else if (N > 0)
+    begin
+        if (q[0])
+        begin
+            {c,acc} = acc+m;
+        end
+        {c,acc,q} = {c,acc,q} >> 1;
+        N = N-1;
+    end
+    else
+    out = {acc,q};
+end 
+endmodule
