1a535e1ca0a4362ea666e551d9af78fb86bb7eca
VV-Sandeep <149654654+VV-Sandeep@users.noreply.github.com>
2024-09-10 16:10:09 +0530

    Update Booth_multiplier_tb.v

--- Booth_multiplier/Booth_multiplier_tb.v
+++ Booth_multiplier/Booth_multiplier_tb.v
@@ -1,5 +1,5 @@
 module booth_multiplier_nbit_tb;
-parameter n = 16;
+parameter n = 8;
 reg [n-1:0]a,b;
 reg clk,rst;
 wire [(2*n)-1:0]out;
@@ -8,8 +8,8 @@ initial clk = 0;
 always #5 clk = ~clk;
 initial
 begin
-    a = 16'h886a;
-    b = 16'h5088;
+    a = 16'h6a;
+    b = 16'h88;
     rst = 1;
     #10 rst = 0;
     #200 $finish;
